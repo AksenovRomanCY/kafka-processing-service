4f66fb19a5398d845cb045b9656d8f54ccf83caf
Aksenov Roman <aksenov.nsk.r.a@gmail.com>
2025-05-19 11:40:07 +0300

    feat(celery): add Celery initialization with Redis broker

---
+++ app/celery_app.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+from app.settings import settings
+
+celery_app = Celery("worker", broker=settings.REDIS_BROKER_URL)
+
+celery_app.conf.update(
+    task_track_started=True,
+    task_time_limit=300,
+    result_backend=None,
+)
